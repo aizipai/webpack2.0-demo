203d79352f28671ae5a5447b6c7074e5ac1859a0
zhangzongbao <zhangzongbao1992@163.com>
2017-06-27 09:13:28 +0800

    补全

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebpackPlugin  = require('html-webpack-plugin')
 
 
 module.exports = {
 	entry: './src/main.js',//入口文件  
 	output: {
 		path:path.resolve(__dirname, 'build'),  //打包后文件路径
-		filename: '[name].js' //打包后文件名称-->可生成多文件
+		filename: 'bundle.js' //打包后文件名称-->可生成多文件
 	},
 	//开始使用loader
 	module: {
@@ -25,7 +26,7 @@ module.exports = {
 			},
 			{
 				test: /\.styl(us)?$/,
-				use: ['style-css','css-loader',{
+				use: ['style-loader','css-loader',{
 					loader: 'postcss-loader',
 					options: {
 						plugins: function(){
@@ -51,6 +52,15 @@ module.exports = {
         contentBase: "./", // 本地服务器所加载的页面所在的目录
         historyApiFallback: true, // 不跳转
         inline: true // 实时刷新
-    }
+    },
+    plugins: [
+    	new webpack.HotModuleReplacementPlugin({
+        	filename: 'index.html',
+        	title:'flex-demo'
+        }), // 热加载插件----------------------------->使用这个插件后才会实现自动更新
+    	new HtmlWebpackPlugin({
+    		template: './index.html'
+    	})
+    ]
 
 }
